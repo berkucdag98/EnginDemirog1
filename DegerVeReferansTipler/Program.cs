d0b4348f7a68c18168eba07f6ea0040af44074c7
berkucdag <berkda3@gmail.com>
2021-01-17 16:26:53 +0300

    V1.1

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -11,6 +11,14 @@ namespace DegerVeReferansTipler
             sayi1 = sayi2;
             sayi2 = 65;
             Console.WriteLine(sayi1);
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar2[0]);
+
             Console.ReadLine();
         }
     }
